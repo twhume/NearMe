e23ac83071b7536987ea44ecab4b4c77d29ad9ad
Tom Hume <twhume@gmail.com>
2011-10-31 18:52:24 +0000

    First commit of example Location Server

---
+++ LocationServer/src/ExampleServer.java
@@ -0,0 +1,17 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.servlet.ServletHolder;
+
+
+public class ExampleServer {
+
+	public static void main(String[] argv) throws Exception {
+		Server server = new Server(8080);
+		Context root = new Context(server,"/",Context.SESSIONS);
+		root.addServlet(new ServletHolder(new LocationReceiverServlet("Receiver")), "/location");
+		server.start();
+	}
+}
---
+++ LocationServer/src/LocationReceiverServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.http.HttpServlet;
+
+
+public class LocationReceiverServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5772869374550462861L;
+
+	public LocationReceiverServlet(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
