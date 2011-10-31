1a5b2e7a25f02d810d183759bfbbeb157b59a927
Tom Hume <twhume@gmail.com>
2011-10-31 20:36:59 +0000

    Commented server code

--- LocationServer/src/ExampleServer.java
+++ LocationServer/src/ExampleServer.java
@@ -2,13 +2,21 @@ import org.mortbay.jetty.Server;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
 
+/**
+ * The simplest web server I could construct, just for test purposes.
+ * Uses the Jetty library to listen on a port and pass hits to /location
+ * through to a simple servlet.
+ * 
+ * @author twhume
+ *
+ */
 
 public class ExampleServer {
 
 	public static void main(String[] argv) throws Exception {
 		Server server = new Server(8080);
 		Context root = new Context(server,"/",Context.SESSIONS);
-		root.addServlet(new ServletHolder(new LocationReceiverServlet("Receiver")), "/location");
+		root.addServlet(new ServletHolder(new LocationReceiverServlet()), "/location");
 		server.start();
 	}
 }
--- LocationServer/src/LocationReceiverServlet.java
+++ LocationServer/src/LocationReceiverServlet.java
@@ -5,28 +5,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Really simple servlet that just dumps specific parameters it's sent to stderr
+ * 
+ * @author twhume
+ *
+ */
 
 public class LocationReceiverServlet extends HttpServlet {
 
-	String[] PARAMS = {"id","latitude","longitude","time"};
+	/* These are the names of the parameters we're interested in */
+	private static String[] PARAMS = {"id","latitude","longitude","time"};
 
 	private static final long serialVersionUID = -5772869374550462861L;
 
-	public LocationReceiverServlet(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
+	/**
+	 * Make GET pass things onto POST. The only reason we do this is to make it easier
+	 * to debug; you can pass parameters into a GET by appending them onto the URL
+	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		doPost(req, resp);
 	}
 
-	@Override
+	/**
+	 * Ultra-simple POST method. Just print out all the relevant parameters we receive.
+	 */
+	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
-
-			
 			for (String param: PARAMS) {
 				System.err.println(param + " => " + req.getParameter(param));
 			}
