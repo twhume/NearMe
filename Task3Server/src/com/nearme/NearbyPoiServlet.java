f0e56c1791dd97df7d9dc0f688c67655e63982e9
Tom Hume <twhume@gmail.com>
2011-11-05 16:36:32 +0000

    Plugged the DummyPoiFinder into the NearbyPoiServlet and hooked it up to GSon so we're serialising Pois to JSON.

--- Task3Server/src/com/nearme/NearbyPoiServlet.java
+++ Task3Server/src/com/nearme/NearbyPoiServlet.java
@@ -1,11 +1,15 @@
 package com.nearme;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * This servlet responds to HTTP GET requests of the form
@@ -30,11 +34,25 @@ public class NearbyPoiServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("application/json");
-		
 		PoiQuery pq = new PoiQuery(req.getPathInfo());
+
+/*
+  		For now, spit out a list of dummy points, always the same.
+  		Later we'll plug the database in and get real ones out.
+ 
 		PoiFinder pf = new DatabasePoiFinder(null);
+*/		PoiFinder pf = new DummyPoiFinder();
 		List<Poi> points = pf.find(pq);
+
+		/* Use GSon to serialise this list onto a JSON structure, and send it to the client.
+		 * This is a little bit complicated because we're asking it to serialise a list of stuff;
+		 * see the manual at https://sites.google.com/site/gson/gson-user-guide#TOC-Collections-Examples
+		 * if you /really/ want to find out why...
+		 */
 		
+		Gson gson = new Gson();
+		Type listOfPois = (Type) (new TypeToken<List<Poi>>(){}).getType();
+		res.getOutputStream().print(gson.toJson(points, listOfPois));
 	}
 	
 	
