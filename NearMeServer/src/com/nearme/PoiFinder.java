51e60f872791094eae71f43f812e7b638276d418
marianarojas <M.Rojas-Morao@sussex.ac.uk>
2011-11-09 09:18:32 +0000

    connection with database

---
+++ NearMeServer/src/com/nearme/PoiFinder.java
@@ -0,0 +1,14 @@
+package com.nearme;
+
+/**
+ * Interface into the PoiFinder.
+ * 
+ * This is an interface and not a class so that we can have two implementations: one which we use "for real", which
+ * talks to a MySQL database, and another dummy implementation which we can use when constructing unit tests
+ * 
+ */
+import java.util.List;
+
+public interface PoiFinder {
+	public List<Poi> find(PoiQuery pq);
+}
---
+++ NearMeServer/src/com/nearme/PoiQuery.java
@@ -0,0 +1,78 @@
+package com.nearme;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * Encapsulates a query for points of interest.
+ * Initially this'll just be lat, long and radius.
+ * 
+ * Later it might include types of POIs to search for.
+ *
+ * @author twhume
+ *
+ */
+public class PoiQuery {
+
+	private double latitude;			/* Latitude of the position to search from */
+	private double longitude;			/* Longitude of the position to search from */
+	private int radius;					/* Metres from the current position to search within */
+	private ArrayList<Integer> types;	/* List of Integers corresponding to the type IDs we are interested in */
+	
+	public PoiQuery(String path) {
+		System.err.println(path);
+		try {
+			StringTokenizer st = new StringTokenizer(path, "/?=");
+			this.latitude = Double.parseDouble(st.nextToken());
+			this.longitude = Double.parseDouble(st.nextToken());
+			this.radius = Integer.parseInt(st.nextToken());
+			this.types = new ArrayList<Integer>();
+
+			/**
+			 * Now parse any ?t= suffix at the end of the URL to extract a list of requested types
+			 */
+			
+			if (st.hasMoreTokens()) {
+				String nextTok = st.nextToken();
+				if (nextTok.equals("t")) {
+					StringTokenizer typeTok = new StringTokenizer(st.nextToken(), ",");
+					while (typeTok.hasMoreTokens()) {
+						nextTok = typeTok.nextToken();
+						types.add(new Integer(Integer.parseInt(nextTok)));
+					}
+				} else System.err.println(nextTok);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("parsing " + path + " threw " + e);
+		}
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public int getRadius() {
+		return radius;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	public ArrayList<Integer> getTypes() {
+		return types;
+	}
+
+	public void setTypes(ArrayList<Integer> types) {
+		this.types = types;
+	}
+	
+	
+}
