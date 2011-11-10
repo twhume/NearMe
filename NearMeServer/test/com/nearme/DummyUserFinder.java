bc0da36c688e0396b59787c5cdeee156e6ade7b1
Tom Hume <twhume@gmail.com>
2011-11-10 16:07:59 +0000

    Unit tests and supporting code for data model stuff.

---
+++ NearMeServer/test/com/nearme/DummyUserFinder.java
@@ -0,0 +1,54 @@
+package com.nearme;
+
+/**
+ * For testing purposes: a sample UserFinder implementation that contains a single user
+ * with an address-book of 3 friends.
+ * 
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class DummyUserFinder implements UserFinder {
+	private User testUserWithBook = null;
+	private User testUserWithoutBook = null;
+	private List<AddressBookEntry> addressBook = null;
+	
+	public DummyUserFinder() {
+		testUserWithBook = new User(1, "android-123456", "hash-1234567890", new Position(-123.45, 67.89));
+		testUserWithoutBook = new User(2, "android-7890", "hash-0987654321", new Position(-123.45, 67.89));
+		addressBook = new ArrayList<AddressBookEntry>();
+		addressBook.add(new AddressBookEntry(1, testUserWithBook, "Tom", AddressBookEntry.PERM_HIDDEN, null));
+		addressBook.add(new AddressBookEntry(2, testUserWithBook, "Dick", AddressBookEntry.PERM_HIDDEN, null));
+		addressBook.add(new AddressBookEntry(3, testUserWithBook, "Harry", AddressBookEntry.PERM_HIDDEN, null));
+	}
+	
+	public User read(int i) {
+		switch (i) {
+			case 1: return testUserWithBook;
+			case 2: return testUserWithoutBook;
+			default: return null;
+		}
+	}
+
+	public User readByHash(String string) {
+		if (string.equals("hash-1234567890")) return testUserWithBook;
+		else if (string.equals("hash-0987654321")) return testUserWithoutBook;
+		return null;
+	}
+
+	public Object readByDeviceId(String string) {
+		if (string.equals("android-123456")) return testUserWithBook;
+		else if (string.equals("android-7890")) return testUserWithoutBook;
+		return null;
+	}
+
+	@Override
+	public List<AddressBookEntry> getAddressBook(int i) {
+		switch (i) {
+			case 1: return addressBook;
+			case 2: return new ArrayList<AddressBookEntry>();
+			default:	return null;
+		}
+	}
+
+}
---
+++ NearMeServer/test/com/nearme/UserFinderTest.java
@@ -0,0 +1,54 @@
+package com.nearme;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserFinderTest {
+	
+	private UserFinder uf = null;
+	
+	@Before
+	public void setUp() {
+		uf = new DummyUserFinder();
+	}
+
+	@Test
+	public void testReadUser() {
+		User testUser = new User(1, "android-123456", "hash-1234567890", new Position(-123.45, 67.89));
+
+		assertEquals(testUser, uf.read(1));
+		
+		assertEquals(testUser, uf.readByHash("hash-1234567890"));
+		assertNull(uf.readByHash("non-existent-hash"));
+		assertNull(uf.readByHash("android-123456")); 
+
+		assertEquals(testUser, uf.readByDeviceId("android-123456"));
+		assertNull(uf.readByDeviceId("hash-1234567890"));
+		assertNull(uf.readByDeviceId("non-existent-id"));
+	}
+
+	@Test
+	public void testReadAddressBookNonExistentUser() {
+		List<AddressBookEntry> book = uf.getAddressBook(12345); // non-existent user
+		assertNull(book);
+	}
+
+	@Test
+	public void testReadAddressBookEmpty() {
+		List<AddressBookEntry> book = uf.getAddressBook(2); // existing user, no book
+		assertEquals(0, book.size());
+	}
+
+	@Test
+	public void testReadAddressBookFull() {
+		List<AddressBookEntry> book = uf.getAddressBook(1); // existing user, with book
+		assertEquals(3, book.size());
+	}
+
+}
