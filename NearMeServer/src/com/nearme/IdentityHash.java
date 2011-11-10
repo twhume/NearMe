6e929b56cb2502216ac1d1ce534503b872fddb07
Tom Hume <twhume@gmail.com>
2011-11-10 16:10:00 +0000

    Added some contents for the IdentityHash

--- NearMeServer/src/com/nearme/IdentityHash.java
+++ NearMeServer/src/com/nearme/IdentityHash.java
@@ -1,5 +1,24 @@
 package com.nearme;
 
-public class IdentityHash {
+/**
+ * An IdentityHash is a phone number which has been run through SHA-1 or similar. We do this so that the
+ * server can keep track of lots of phone numbers and use them to identify users uniquely, without actually
+ * knowing what those phone numbers are. It's a privacy thang.
+ * 
+ * @author twhume
+ *
+ */
 
+public class IdentityHash {
+	private String hash;
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+	
+	
 }
