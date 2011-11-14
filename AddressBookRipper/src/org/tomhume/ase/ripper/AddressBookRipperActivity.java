43db9f439012c7e69818724f111809df9318f33d
Tom Hume <twhume@gmail.com>
2011-11-14 08:57:36 +0000

    AddressBookRipper added
    I'll want to work in a separate app to get the contact management stuff
    working. This is that app.

---
+++ AddressBookRipper/src/org/tomhume/ase/ripper/AddressBookRipperActivity.java
@@ -0,0 +1,13 @@
+package org.tomhume.ase.ripper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddressBookRipperActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
