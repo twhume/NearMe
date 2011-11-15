b9fd3bba0c0ee8687d33984a01c34035d3167132
Tom Hume <twhume@gmail.com>
2011-11-15 10:51:02 +0000

    Fixed up logic so all numbers are read correctly and assigned to correct contacts.

--- AddressBookRipper/src/org/tomhume/ase/ripper/AddressBookRipperActivity.java
+++ AddressBookRipper/src/org/tomhume/ase/ripper/AddressBookRipperActivity.java
@@ -2,14 +2,14 @@ package org.tomhume.ase.ripper;
 
 import java.util.ArrayList;
 
+import com.google.gson.Gson;
+
 import android.app.Activity;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.AsyncTask.Status;
 import android.os.Bundle;
-import android.provider.Contacts.People;
-import android.provider.ContactsContract.CommonDataKinds;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.Settings.Secure;
 import android.util.Log;
@@ -18,93 +18,133 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class AddressBookRipperActivity extends Activity {
-	
+
 	private static final String TAG = "Ripper";
-	private UploadContactsTask uploader = null;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        Button ripButton = (Button)this.findViewById(R.id.btnRip);
-        uploader = new UploadContactsTask();
-        
-        ripButton.setOnClickListener(new OnClickListener() {
-          @Override
-          public void onClick(View v) {
-        	  Log.i(TAG, "CLICK!");
-        	  
-        	  if (uploader.getStatus().equals(Status.RUNNING)) return;
-        	  if (uploader.getStatus().equals(Status.FINISHED)) uploader = new UploadContactsTask();
-        	  uploader.execute();
+	private GatherContactsTask gatherer = null;
 
-          }
-        });
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button ripButton = (Button) this.findViewById(R.id.btnRip);
+		gatherer = new GatherContactsTask();
+
+		ripButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG, "CLICK!");
+
+				if (gatherer.getStatus().equals(Status.RUNNING))
+					return;
+				if (gatherer.getStatus().equals(Status.FINISHED))
+					gatherer = new GatherContactsTask();
+				gatherer.execute();
+
+			}
+		});
+	}
+
+	private void uploadContacts(AddressBook a) {
+    	UploadContactsTask uploader = new UploadContactsTask();
+    	uploader.execute(a);
     }
-    
-    private class UploadContactsTask extends AsyncTask<Void, Integer, Boolean> {
 
-        protected void onProgressUpdate(Integer... progress) {
-        	Log.i(TAG, System.currentTimeMillis() + " progress, current= "+ progress[0].intValue());
-        }
-
-        protected void onPostExecute(Boolean result) {
-        	Log.i(TAG, System.currentTimeMillis() +  " done, result= "+ result);
-        }
+	private class UploadContactsTask extends AsyncTask<AddressBook, Integer, Boolean> {
 
 		@Override
-		protected Boolean doInBackground(Void... params) {
-			Log.i(TAG, System.currentTimeMillis() + " starting");
+		protected Boolean doInBackground(AddressBook... abe) {
+			Gson gson = new Gson();
+			Log.i(TAG, "Got entries " + abe[0].getEntries().size());
+			System.err.println(gson.toJson(abe[0]));
 
-      	  String[] projection = new String[] {
-    			  Phone._ID,
-                  Phone.DISPLAY_NAME,
-                  Phone.NUMBER,
-                  Phone.RAW_CONTACT_ID
-               };
-    	  Uri contacts =  Phone.CONTENT_URI;
-    	  Cursor managedCursor = managedQuery(contacts,
-                  projection, // Which columns to return 
-                  null,       // Which rows to return (all rows)
-                  null,       // Selection arguments (none)
-                  // Put the results in ascending order by name
-                  Phone.DISPLAY_NAME + " ASC");
-    	  
-    	  AddressBook a = new AddressBook();
-    	  a.setDeviceId(Secure.getString(getApplicationContext().getContentResolver(),Secure.ANDROID_ID));
-    	  ArrayList<AddressBookEntry> entries = new ArrayList<AddressBookEntry>();
-    	  ArrayList<String> hashes = new ArrayList<String>();
-    	  String lastId = null;
-    	  
-    	  try {
-    		  managedCursor.moveToFirst();
-    		  lastId = managedCursor.getString(0);
-    		  do {
-        		  Log.i(TAG, managedCursor.getString(3) + ": "+ managedCursor.getString(1) + "=>" + managedCursor.getString(2));
-
-        		  hashes.add(managedCursor.getString(2));
-        		  
-        		  if (!managedCursor.getString(3).equals(lastId)) {
-	        		  AddressBookEntry abe = new AddressBookEntry();
-	        		  abe.setName(managedCursor.getString(1));
-	        		  abe.setHashes(hashes);
-	        		  entries.add(abe);
-	        		  hashes = new ArrayList<String>();
-        		  }
-        		  
-    		  } while (managedCursor.moveToNext());
-    	  } finally {
-    		  managedCursor.close();
-    	  }
-			
-    	  a.setEntries(entries);
-    	  Log.i(TAG, "First="+ entries.get(0).getName());
-    	  Log.i(TAG, "Last="+ entries.get(entries.size()-1).getName());
-    	  
 			return true;
 		}
-    }
-    
+	}
+
+	
+	/**
+	 * Private class to gather contacts from the on-phone address book and use
+	 * them to populate an AddressBook object in the background.
+	 * 
+	 * @author twhume
+	 * 
+	 */
+
+	private class GatherContactsTask extends AsyncTask<Void, Integer, AddressBook> {
+
+		protected void onProgressUpdate(Integer... progress) {
+			Log.i(TAG, System.currentTimeMillis() + " progress, current= "
+					+ progress[0].intValue());
+			// TODO implement a callback into a progress bar, if this task takes  any time on slow phones
+		}
+
+		/**
+		 * Once we're done, trigger the uploading process
+		 */
+
+		protected void onPostExecute(AddressBook result) {
+			Log.i(TAG, System.currentTimeMillis() + " done, result= " + result);
+			uploadContacts(result);
+		}
+
+		/**
+		 * Iterate through the address book, creating an AddressBook structure which
+		 * contains a list of AddressBookEntries, which each contains a list of
+		 * hashes
+		 */
+		
+		@Override
+		protected AddressBook doInBackground(Void... params) {
+			Log.i(TAG, System.currentTimeMillis() + " starting");
+
+			String[] columns = new String[] { Phone._ID, Phone.DISPLAY_NAME,
+					Phone.NUMBER, Phone.RAW_CONTACT_ID };
+			Uri contacts = Phone.CONTENT_URI;
+			Cursor managedCursor = managedQuery(contacts, columns,
+					null, // get all rows
+					null, // Selection arguments (none)
+					// Put the results in ascending order by name
+					Phone.DISPLAY_NAME + " ASC");
+
+			AddressBook a = new AddressBook();
+			a.setDeviceId(Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID));
+			ArrayList<AddressBookEntry> entries = new ArrayList<AddressBookEntry>();
+			ArrayList<String> hashes = new ArrayList<String>();
+			String lastId = null;
+
+			try {
+				managedCursor.moveToFirst();
+				lastId = managedCursor.getString(3);
+				AddressBookEntry abe = new AddressBookEntry();
+				abe.setName(managedCursor.getString(1));
+				
+				do {
+
+					if (!managedCursor.getString(3).equals(lastId)) {
+						abe.setHashes(hashes);
+						entries.add(abe);
+						abe = new AddressBookEntry();
+						abe.setName(managedCursor.getString(1));
+						hashes = new ArrayList<String>();
+					}
+					//TODO actually hash this so that we're not delivering numbers to the server
+					hashes.add(managedCursor.getString(2)); 
+					lastId = managedCursor.getString(3);
+
+				} while (managedCursor.moveToNext());
+				
+				abe.setHashes(hashes);
+				entries.add(abe);
+
+			} finally {
+				managedCursor.close();
+			}
+
+			a.setEntries(entries);
+			return a;
+		}
+	}
+
 }
