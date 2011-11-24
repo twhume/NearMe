5b979c29b733d2d44274ae9f74dda08973f5d818
Alan Donohoe <A.Donohoe@sussex.ac.uk>
2011-11-24 22:20:40 +0000

    Added AddressBook, AddressBookEntry and libphonenumber. jar to project

---
+++ AdvSoftEngApp1/src/com/advsofteng/app1/AddressBook.java
@@ -0,0 +1,28 @@
+package com.advsofteng.app1;
+
+import java.util.List;
+
+public class AddressBook {
+	private String deviceId;
+	private String msisdnHash;
+	private List<AddressBookEntry> entries;
+	
+	public String getDeviceId() {
+		return deviceId;
+	}
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+	public String getOwnerHash() {
+		return msisdnHash;
+	}
+	public void setOwnerHash(String ownerHash) {
+		this.msisdnHash = ownerHash;
+	}
+	public List<AddressBookEntry> getEntries() {
+		return entries;
+	}
+	public void setEntries(List<AddressBookEntry> entries) {
+		this.entries = entries;
+	}
+}
---
+++ AdvSoftEngApp1/src/com/advsofteng/app1/AddressBookEntry.java
@@ -0,0 +1,26 @@
+package com.advsofteng.app1;
+
+import java.util.List;
+
+public class AddressBookEntry {
+	private String name;
+	private List<String> hashes;
+	
+	public static final int PERM_HIDDEN = 0; /* indicates user wishes to hide from this contact */
+	public static final int PERM_SHOWN = 1; /* indicates user is happy to be seen by this contact */
+
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<String> getHashes() {
+		return hashes;
+	}
+	public void setHashes(List<String> hashes) {
+		this.hashes = hashes;
+	}
+
+}
